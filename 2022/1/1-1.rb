52cefb1d582081c2a57f122f3883c8726d442dd2
Lloyd Dilley <ldilley@users.noreply.github.com>
2023-11-20 20:46:15 -0500

    Rename day 1 to include part 1

--- 2022/1/1-1.rb
+++ 2022/1/1-1.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# Advent of Code 2022 day 1 solution in Ruby
+# Advent of Code 2022 day 1 part 1 solution in Ruby
 # http://adventofcode.com/2022/day/1
 # Lloyd Dilley
 
@@ -10,10 +10,10 @@ begin
   current_total = 0
   largest_total = 0
   elf_number = 1
-  wealthiest_elf = 1
+  wealthiest_elf = 0
   File.open(FILE_NAME).each_line do |line|
     if line.chomp.empty?
-      #puts("Last total: #{current_total}")
+      #puts("Last total: #{current_total} calories")
       if current_total > largest_total
         largest_total = current_total
         wealthiest_elf = elf_number
@@ -23,11 +23,11 @@ begin
       #puts("Elf number: ##{elf_number}")
     else
       current_total += line.to_i
-      #puts("Current total: #{current_total}")
+      #puts("Current total: #{current_total} calories")
     end
   end
   puts("Wealthiest elf: ##{wealthiest_elf}")
-  puts("Largest total: #{largest_total}")
+  puts("Largest total: #{largest_total} calories")
 rescue Exception => e
   puts("Unable to parse input file: #{e.message}")
 end
